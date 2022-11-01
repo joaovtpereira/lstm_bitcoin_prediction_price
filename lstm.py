a872bb628ef6a9f5bd9e481dc9a3e0e92a49c39d
joaovtpereira <joaovitorpessoal2@gmail.com>
2022-11-01 08:16:10 -0300

    view the data in graph to 2014

--- lstm.py
+++ lstm.py
@@ -33,5 +33,96 @@ from plotly.subplots import make_subplots
 
 maindf=pd.read_csv('BTC-USD.csv')
 
-print('Total number of days present in the dataset: ',maindf.shape[0])
-print('Total number of fields present in the dataset: ',maindf.shape[1])
+# 2968 days
+# 7 fields per day
+# Shape (2713, 7)
+
+# Fields: Date, Open, High, Low, Close, Adj Close, Volume
+
+# We using Date and Close prioce because in this case its not necessary other values in times series
+
+# Checking for null vallues
+
+print('Null Values:',maindf.isnull().values.sum())
+
+print('NA values:',maindf.isnull().values.any())
+
+# Printing the start date and End date of the dataset
+
+sd=maindf.iloc[0][0]
+ed=maindf.iloc[-1][0]
+
+print('Starting Date',sd)
+print('Ending Date',ed)
+
+# Analysis of the year 2014
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+# filter date per year
+y_2014 = maindf.loc[(maindf['Date'] >= '2014-09-17') & (maindf['Date'] < '2014-12-31')]
+
+# Remove other columns
+y_2014.drop(y_2014[['Adj Close','Volume']], inplace = True, axis=1)
+
+# Agroup values of close and open to mean per mounth
+monthvise= y_2014.groupby(y_2014['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+
+fig = go.Figure()
+
+fig.add_trace(go.Bar(
+    x=monthvise.index,
+    y=monthvise['Open'],
+    name='Stock Open Price',
+    marker_color='crimson'
+))
+fig.add_trace(go.Bar(
+    x=monthvise.index,
+    y=monthvise['Close'],
+    name='Stock Close Price',
+    marker_color='lightsalmon'
+))
+
+fig.update_layout(barmode='group', xaxis_tickangle=-45, 
+                  title='Monthwise comparision between Stock open and close price')
+fig.show()
+
+
+# Agroup values of high and low mean per mounth
+
+y_2014.groupby(y_2014['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2014.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_low = y_2014.groupby(y_2014['Date'].dt.strftime('%B'))['Low'].min()
+
+fig = go.Figure()
+fig.add_trace(go.Bar(
+    x=monthvise_high.index,
+    y=monthvise_high,
+    name='Stock high Price',
+    marker_color='rgb(0, 153, 204)'
+))
+fig.add_trace(go.Bar(
+    x=monthvise_low.index,
+    y=monthvise_low,
+    name='Stock low Price',
+    marker_color='rgb(255, 128, 0)'
+))
+
+fig.update_layout(barmode='group', 
+                  title=' Monthwise High and Low stock price')
+fig.show()
+
+
+# Fig values ind 2014 graph
+
+names = cycle(['Stock Open Price','Stock Close Price','Stock High Price','Stock Low Price'])
+
+fig = px.line(y_2014, x=y_2014.Date, y=[y_2014['Open'], y_2014['Close'], 
+                                          y_2014['High'], y_2014['Low']],
+             labels={'Date': 'Date','value':'Stock value'})
+fig.update_layout(title_text='Stock analysis chart', font_size=15, font_color='black',legend_title_text='Stock Parameters')
+fig.for_each_trace(lambda t:  t.update(name = next(names)))
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+
+fig.show()
