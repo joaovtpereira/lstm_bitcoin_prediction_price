5894bd6ff44bd4e94c26d3040c939ac7d2bb927e
joaovtpereira <joaovitorpessoal2@gmail.com>
2022-11-01 07:34:39 -0300

    add imports

--- lstm.py
+++ lstm.py
@@ -1,3 +1,28 @@
-import pandas as pd, numpy as np, matplotlib.pyplot as plt
+import os
+import pandas as pd
+import numpy as np
+import math
+import datetime as dt
+import matplotlib.pyplot as plt
 
+# For Evalution we will use these library
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score, r2_score 
+from sklearn.metrics import mean_poisson_deviance, mean_gamma_deviance, accuracy_score
 from sklearn.preprocessing import MinMaxScaler
+
+# For model building we will use these library
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LSTM
+
+
+# For PLotting we will use these library
+
+import matplotlib.pyplot as plt
+from itertools import cycle
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
