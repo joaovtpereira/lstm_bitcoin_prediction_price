dbffbd230ae43efaa0a9f8b648e6c50184529e81
joaovtpereira <joaovitorpessoal2@gmail.com>
2022-11-07 07:46:17 -0300

    analise anaos 2024-2022

--- lstm.py
+++ lstm.py
@@ -68,61 +68,187 @@ y_2014.drop(y_2014[['Adj Close','Volume']], inplace = True, axis=1)
 # Agroup values of close and open to mean per mounth
 monthvise= y_2014.groupby(y_2014['Date'].dt.strftime('%B'))[['Open','Close']].mean()
 
-fig = go.Figure()
-
-fig.add_trace(go.Bar(
-    x=monthvise.index,
-    y=monthvise['Open'],
-    name='Stock Open Price',
-    marker_color='crimson'
-))
-fig.add_trace(go.Bar(
-    x=monthvise.index,
-    y=monthvise['Close'],
-    name='Stock Close Price',
-    marker_color='lightsalmon'
-))
-
-fig.update_layout(barmode='group', xaxis_tickangle=-45, 
-                  title='Monthwise comparision between Stock open and close price')
-fig.show()
-
-
 # Agroup values of high and low mean per mounth
 
 y_2014.groupby(y_2014['Date'].dt.strftime('%B'))['Low'].min()
 monthvise_high = y_2014.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
 monthvise_low = y_2014.groupby(y_2014['Date'].dt.strftime('%B'))['Low'].min()
 
-fig = go.Figure()
-fig.add_trace(go.Bar(
-    x=monthvise_high.index,
-    y=monthvise_high,
-    name='Stock high Price',
-    marker_color='rgb(0, 153, 204)'
-))
-fig.add_trace(go.Bar(
-    x=monthvise_low.index,
-    y=monthvise_low,
-    name='Stock low Price',
-    marker_color='rgb(255, 128, 0)'
-))
+# Analysis of the year 2015
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
 
-fig.update_layout(barmode='group', 
-                  title=' Monthwise High and Low stock price')
-fig.show()
+y_2015 = maindf.loc[(maindf['Date'] >= '2015-01-01')
+                     & (maindf['Date'] < '2016-01-01')]
+
+y_2015.drop(y_2015[['Adj Close','Volume']],axis=1)
 
 
-# Fig values ind 2014 graph
+monthvise= y_2015.groupby(y_2015['Date'].dt.strftime('%B'))[['Open','Close']].mean()
 
-names = cycle(['Stock Open Price','Stock Close Price','Stock High Price','Stock Low Price'])
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
 
-fig = px.line(y_2014, x=y_2014.Date, y=[y_2014['Open'], y_2014['Close'], 
-                                          y_2014['High'], y_2014['Low']],
-             labels={'Date': 'Date','value':'Stock value'})
-fig.update_layout(title_text='Stock analysis chart', font_size=15, font_color='black',legend_title_text='Stock Parameters')
-fig.for_each_trace(lambda t:  t.update(name = next(names)))
-fig.update_xaxes(showgrid=False)
-fig.update_yaxes(showgrid=False)
 
-fig.show()
+y_2015.groupby(y_2015['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2015.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_high = monthvise_high.reindex(new_order, axis=0)
+
+monthvise_low = y_2015.groupby(y_2015['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_low = monthvise_low.reindex(new_order, axis=0)
+
+# Analysis of the year 2016
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2016 = maindf.loc[(maindf['Date'] >= '2016-01-01')
+                     & (maindf['Date'] < '2017-01-01')]
+
+y_2016.drop(y_2016[['Adj Close','Volume']],axis=1)
+
+monthvise= y_2016.groupby(y_2016['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+
+y_2016.groupby(y_2016['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2016.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_high = monthvise_high.reindex(new_order, axis=0)
+
+monthvise_low = y_2016.groupby(y_2016['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_low = monthvise_low.reindex(new_order, axis=0)
+
+# Analysis of the year 2017
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2017 = maindf.loc[(maindf['Date'] >= '2017-01-01')
+                     & (maindf['Date'] < '2018-01-01')]
+
+y_2017.drop(y_2017[['Adj Close','Volume']],axis=1)
+
+monthvise= y_2017.groupby(y_2017['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+
+y_2017.groupby(y_2017['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2017.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_high = monthvise_high.reindex(new_order, axis=0)
+
+monthvise_low = y_2017.groupby(y_2017['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_low = monthvise_low.reindex(new_order, axis=0)
+
+# Analysis of the year 2018
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2018 = maindf.loc[(maindf['Date'] >= '2018-01-01')
+                     & (maindf['Date'] < '2019-01-01')]
+
+y_2018.drop(y_2018[['Adj Close','Volume']],axis=1)
+
+monthvise= y_2018.groupby(y_2018['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+
+y_2018.groupby(y_2018['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2018.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_high = monthvise_high.reindex(new_order, axis=0)
+
+monthvise_low = y_2018.groupby(y_2018['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_low = monthvise_low.reindex(new_order, axis=0)
+
+# Analysis of the year 2019
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2019 = maindf.loc[(maindf['Date'] >= '2019-01-01')
+                     & (maindf['Date'] < '2020-01-01')]
+
+y_2019.drop(y_2019[['Adj Close','Volume']],axis=1)
+
+monthvise= y_2019.groupby(y_2019['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+
+y_2019.groupby(y_2019['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2019.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_high = monthvise_high.reindex(new_order, axis=0)
+
+monthvise_low = y_2019.groupby(y_2019['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_low = monthvise_low.reindex(new_order, axis=0)
+
+# Analysis of the year 2020
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2020 = maindf.loc[(maindf['Date'] >= '2020-01-01')
+                     & (maindf['Date'] < '2021-01-01')]
+
+y_2020.drop(y_2020[['Adj Close','Volume']],axis=1)
+
+monthvise= y_2020.groupby(y_2020['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+
+y_2020.groupby(y_2020['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2020.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_high = monthvise_high.reindex(new_order, axis=0)
+
+monthvise_low = y_2020.groupby(y_2020['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_low = monthvise_low.reindex(new_order, axis=0)
+
+# Analysis of the year 2021
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2021 = maindf.loc[(maindf['Date'] >= '2021-01-01')
+                     & (maindf['Date'] < '2021-12-31')]
+
+y_2021.drop(y_2021[['Adj Close','Volume']],axis=1)
+
+monthvise= y_2021.groupby(y_2021['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+
+y_2021.groupby(y_2021['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2021.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_high = monthvise_high.reindex(new_order, axis=0)
+
+monthvise_low = y_2021.groupby(y_2021['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_low = monthvise_low.reindex(new_order, axis=0)
+
+# Analysis of the year 2022
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2022 = maindf.loc[(maindf['Date'] >= '2022-01-01')
+                     & (maindf['Date'] < '2022-02-19')]
+
+y_2022.drop(y_2015[['Adj Close','Volume']],axis=1)
+
+monthvise= y_2022.groupby(y_2022['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+
+y_2022.groupby(y_2022['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2022.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_high = monthvise_high.reindex(new_order, axis=0)
+
+monthvise_low = y_2022.groupby(y_2022['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_low = monthvise_low.reindex(new_order, axis=0)
+
+# Overall Analysis from 2014-2022
+
+# https://www.youtube.com/watch?v=p-QY7JNGD60&list=WL&index=3
+# min 16:10
+
+# use to collect data https://finance.yahoo.com/quote/BTC-USD/history?p=BTC-USD
+
+# https://www.kaggle.com/code/meetnagadia/bitcoin-price-prediction-using-lstm/notebook#1.-What-is-LSTM-?
