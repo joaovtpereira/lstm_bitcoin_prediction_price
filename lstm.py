dad9d42ea8eb6c15a84a0dd62fb8960e54ab4e59
joaovtpereira <joaovitorpessoal2@gmail.com>
2022-11-07 08:20:13 -0300

    treinamento e resultados

--- lstm.py
+++ lstm.py
@@ -30,6 +30,14 @@ from plotly.subplots import make_subplots
 # Load our dataset 
 # Note it should be in same dir
 
+def create_dataset(dataset, time_step=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-time_step-1):
+        a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100 
+        dataX.append(a)
+        dataY.append(dataset[i + time_step, 0])
+    return np.array(dataX), np.array(dataY)
+
 
 maindf=pd.read_csv('BTC-USD.csv')
 
@@ -94,9 +102,100 @@ train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closed
 print("train_data: ", train_data.shape)
 print("test_data: ", test_data.shape)
 
-# https://www.youtube.com/watch?v=p-QY7JNGD60&list=WL&index=3
-# min 16:10
+# criando array um time series
+time_step = 15
+X_train, y_train = create_dataset(train_data, time_step)
+X_test, y_test = create_dataset(test_data, time_step)
 
-# use to collect data https://finance.yahoo.com/quote/BTC-USD/history?p=BTC-USD
+# reshape input to be [samples, time steps, features] which is required for LSTM
+X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
+X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
 
-# https://www.kaggle.com/code/meetnagadia/bitcoin-price-prediction-using-lstm/notebook#1.-What-is-LSTM-?
+print("X_train: ", X_train.shape)
+print("X_test: ", X_test.shape)
+
+# Criando Modelo
+
+model=Sequential()
+
+model.add(LSTM(10,input_shape=(None,1),activation="relu"))
+
+model.add(Dense(1))
+
+model.compile(loss="mean_squared_error",optimizer="adam")
+
+history = model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=200,batch_size=32,verbose=1)
+
+### Lets Do the prediction and check performance metrics
+train_predict=model.predict(X_train)
+test_predict=model.predict(X_test)
+train_predict.shape, test_predict.shape
+
+# Transform back to original form
+
+train_predict = scaler.inverse_transform(train_predict)
+test_predict = scaler.inverse_transform(test_predict)
+original_ytrain = scaler.inverse_transform(y_train.reshape(-1,1)) 
+original_ytest = scaler.inverse_transform(y_test.reshape(-1,1)) 
+
+# Evaluation metrices RMSE and MAE
+print("Train data RMSE: ", math.sqrt(mean_squared_error(original_ytrain,train_predict)))
+print("Train data MSE: ", mean_squared_error(original_ytrain,train_predict))
+print("Train data MAE: ", mean_absolute_error(original_ytrain,train_predict))
+print("-------------------------------------------------------------------------------------")
+print("Test data RMSE: ", math.sqrt(mean_squared_error(original_ytest,test_predict)))
+print("Test data MSE: ", mean_squared_error(original_ytest,test_predict))
+print("Test data MAE: ", mean_absolute_error(original_ytest,test_predict))
+
+# Variance Regression Score
+
+print("Train data explained variance regression score:", 
+      explained_variance_score(original_ytrain, train_predict))
+print("Test data explained variance regression score:", 
+      explained_variance_score(original_ytest, test_predict))
+
+# R square score for regression
+
+print("Train data R2 score:", r2_score(original_ytrain, train_predict))
+print("Test data R2 score:", r2_score(original_ytest, test_predict))
+
+# Regression Loss Mean Gamma deviance regression loss (MGD) and Mean Poisson deviance regression loss (MPD)
+
+print("Train data MGD: ", mean_gamma_deviance(original_ytrain, train_predict))
+print("Test data MGD: ", mean_gamma_deviance(original_ytest, test_predict))
+print("----------------------------------------------------------------------")
+print("Train data MPD: ", mean_poisson_deviance(original_ytrain, train_predict))
+print("Test data MPD: ", mean_poisson_deviance(original_ytest, test_predict))
+
+# Comparision of original stock close price and predicted close price
+
+look_back=time_step
+trainPredictPlot = np.empty_like(closedf)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(train_predict)+look_back, :] = train_predict
+print("Train predicted data: ", trainPredictPlot.shape)
+
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(closedf)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(train_predict)+(look_back*2)+1:len(closedf)-1, :] = test_predict
+print("Test predicted data: ", testPredictPlot.shape)
+
+names = cycle(['Original close price','Train predicted close price','Test predicted close price'])
+
+
+plotdf = pd.DataFrame({'date': close_stock['Date'],
+                       'original_close': close_stock['Close'],
+                      'train_predicted_close': trainPredictPlot.reshape(1,-1)[0].tolist(),
+                      'test_predicted_close': testPredictPlot.reshape(1,-1)[0].tolist()})
+
+fig = px.line(plotdf,x=plotdf['date'], y=[plotdf['original_close'],plotdf['train_predicted_close'],
+                                          plotdf['test_predicted_close']],
+              labels={'value':'Stock price','date': 'Date'})
+fig.update_layout(title_text='Comparision between original close price vs predicted close price',
+                  plot_bgcolor='white', font_size=15, font_color='black', legend_title_text='Close Price')
+fig.for_each_trace(lambda t:  t.update(name = next(names)))
+
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.show()
