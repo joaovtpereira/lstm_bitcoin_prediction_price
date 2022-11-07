fcb35bd7b579db564eaae30b0b0c442cbd268d9f
joaovtpereira <joaovitorpessoal2@gmail.com>
2022-11-07 08:10:52 -0300

    separando base treinamento e teste

--- lstm.py
+++ lstm.py
@@ -55,196 +55,44 @@ ed=maindf.iloc[-1][0]
 print('Starting Date',sd)
 print('Ending Date',ed)
 
-# Analysis of the year 2014
-
-maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
-
-# filter date per year
-y_2014 = maindf.loc[(maindf['Date'] >= '2014-09-17') & (maindf['Date'] < '2014-12-31')]
-
-# Remove other columns
-y_2014.drop(y_2014[['Adj Close','Volume']], inplace = True, axis=1)
-
-# Agroup values of close and open to mean per mounth
-monthvise= y_2014.groupby(y_2014['Date'].dt.strftime('%B'))[['Open','Close']].mean()
-
-# Agroup values of high and low mean per mounth
-
-y_2014.groupby(y_2014['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_high = y_2014.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
-monthvise_low = y_2014.groupby(y_2014['Date'].dt.strftime('%B'))['Low'].min()
-
-# Analysis of the year 2015
-maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
-
-y_2015 = maindf.loc[(maindf['Date'] >= '2015-01-01')
-                     & (maindf['Date'] < '2016-01-01')]
-
-y_2015.drop(y_2015[['Adj Close','Volume']],axis=1)
-
-
-monthvise= y_2015.groupby(y_2015['Date'].dt.strftime('%B'))[['Open','Close']].mean()
-
-new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
-             'September', 'October', 'November', 'December']
-monthvise = monthvise.reindex(new_order, axis=0)
-
-
-y_2015.groupby(y_2015['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_high = y_2015.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
-monthvise_high = monthvise_high.reindex(new_order, axis=0)
-
-monthvise_low = y_2015.groupby(y_2015['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_low = monthvise_low.reindex(new_order, axis=0)
-
-# Analysis of the year 2016
-
-maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
-
-y_2016 = maindf.loc[(maindf['Date'] >= '2016-01-01')
-                     & (maindf['Date'] < '2017-01-01')]
-
-y_2016.drop(y_2016[['Adj Close','Volume']],axis=1)
-
-monthvise= y_2016.groupby(y_2016['Date'].dt.strftime('%B'))[['Open','Close']].mean()
-new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
-             'September', 'October', 'November', 'December']
-monthvise = monthvise.reindex(new_order, axis=0)
-
-y_2016.groupby(y_2016['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_high = y_2016.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
-monthvise_high = monthvise_high.reindex(new_order, axis=0)
-
-monthvise_low = y_2016.groupby(y_2016['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_low = monthvise_low.reindex(new_order, axis=0)
-
-# Analysis of the year 2017
-
-maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
-
-y_2017 = maindf.loc[(maindf['Date'] >= '2017-01-01')
-                     & (maindf['Date'] < '2018-01-01')]
-
-y_2017.drop(y_2017[['Adj Close','Volume']],axis=1)
-
-monthvise= y_2017.groupby(y_2017['Date'].dt.strftime('%B'))[['Open','Close']].mean()
-new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
-             'September', 'October', 'November', 'December']
-monthvise = monthvise.reindex(new_order, axis=0)
-
-y_2017.groupby(y_2017['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_high = y_2017.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
-monthvise_high = monthvise_high.reindex(new_order, axis=0)
-
-monthvise_low = y_2017.groupby(y_2017['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_low = monthvise_low.reindex(new_order, axis=0)
-
-# Analysis of the year 2018
-
-maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
-
-y_2018 = maindf.loc[(maindf['Date'] >= '2018-01-01')
-                     & (maindf['Date'] < '2019-01-01')]
-
-y_2018.drop(y_2018[['Adj Close','Volume']],axis=1)
-
-monthvise= y_2018.groupby(y_2018['Date'].dt.strftime('%B'))[['Open','Close']].mean()
-new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
-             'September', 'October', 'November', 'December']
-monthvise = monthvise.reindex(new_order, axis=0)
-
-y_2018.groupby(y_2018['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_high = y_2018.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
-monthvise_high = monthvise_high.reindex(new_order, axis=0)
-
-monthvise_low = y_2018.groupby(y_2018['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_low = monthvise_low.reindex(new_order, axis=0)
-
-# Analysis of the year 2019
-
-maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
-
-y_2019 = maindf.loc[(maindf['Date'] >= '2019-01-01')
-                     & (maindf['Date'] < '2020-01-01')]
-
-y_2019.drop(y_2019[['Adj Close','Volume']],axis=1)
-
-monthvise= y_2019.groupby(y_2019['Date'].dt.strftime('%B'))[['Open','Close']].mean()
-new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
-             'September', 'October', 'November', 'December']
-monthvise = monthvise.reindex(new_order, axis=0)
-
-y_2019.groupby(y_2019['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_high = y_2019.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
-monthvise_high = monthvise_high.reindex(new_order, axis=0)
-
-monthvise_low = y_2019.groupby(y_2019['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_low = monthvise_low.reindex(new_order, axis=0)
-
-# Analysis of the year 2020
-
-maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
-
-y_2020 = maindf.loc[(maindf['Date'] >= '2020-01-01')
-                     & (maindf['Date'] < '2021-01-01')]
-
-y_2020.drop(y_2020[['Adj Close','Volume']],axis=1)
-
-monthvise= y_2020.groupby(y_2020['Date'].dt.strftime('%B'))[['Open','Close']].mean()
-new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
-             'September', 'October', 'November', 'December']
-monthvise = monthvise.reindex(new_order, axis=0)
-
-y_2020.groupby(y_2020['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_high = y_2020.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
-monthvise_high = monthvise_high.reindex(new_order, axis=0)
-
-monthvise_low = y_2020.groupby(y_2020['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_low = monthvise_low.reindex(new_order, axis=0)
-
-# Analysis of the year 2021
-
-maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
-
-y_2021 = maindf.loc[(maindf['Date'] >= '2021-01-01')
-                     & (maindf['Date'] < '2021-12-31')]
-
-y_2021.drop(y_2021[['Adj Close','Volume']],axis=1)
-
-monthvise= y_2021.groupby(y_2021['Date'].dt.strftime('%B'))[['Open','Close']].mean()
-new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
-             'September', 'October', 'November', 'December']
-monthvise = monthvise.reindex(new_order, axis=0)
-
-y_2021.groupby(y_2021['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_high = y_2021.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
-monthvise_high = monthvise_high.reindex(new_order, axis=0)
-
-monthvise_low = y_2021.groupby(y_2021['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_low = monthvise_low.reindex(new_order, axis=0)
-
-# Analysis of the year 2022
-
-maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
-
-y_2022 = maindf.loc[(maindf['Date'] >= '2022-01-01')
-                     & (maindf['Date'] < '2022-02-19')]
-
-y_2022.drop(y_2015[['Adj Close','Volume']],axis=1)
-
-monthvise= y_2022.groupby(y_2022['Date'].dt.strftime('%B'))[['Open','Close']].mean()
-new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
-             'September', 'October', 'November', 'December']
-monthvise = monthvise.reindex(new_order, axis=0)
-
-y_2022.groupby(y_2022['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_high = y_2022.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
-monthvise_high = monthvise_high.reindex(new_order, axis=0)
-
-monthvise_low = y_2022.groupby(y_2022['Date'].dt.strftime('%B'))['Low'].min()
-monthvise_low = monthvise_low.reindex(new_order, axis=0)
-
 # Overall Analysis from 2014-2022
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_overall = maindf.loc[(maindf['Date'] >= '2014-09-17')
+                     & (maindf['Date'] <= '2022-02-19')]
+
+y_overall.drop(y_overall[['Adj Close','Volume']],axis=1)
+
+monthvise= y_overall.groupby(y_overall['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+
+# Building LSTM Model
+
+closedf = maindf[['Date','Close']]
+print("Shape of close dataframe:", closedf.shape)
+
+# pegando ultimo ano para prever
+closedf = closedf[closedf['Date'] > '2021-11-01']
+close_stock = closedf.copy()
+print("Total data for prediction: ",closedf.shape[0])
+
+# deleting date column and normalizing using MinMax Scaler
+
+del closedf['Date']
+scaler=MinMaxScaler(feature_range=(0,1))
+closedf=scaler.fit_transform(np.array(closedf).reshape(-1,1))
+print(closedf.shape)
+
+# we keep the training set as 60% and 40% testing set
+
+training_size=int(len(closedf)*0.60)
+test_size=len(closedf)-training_size
+train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closedf),:1]
+print("train_data: ", train_data.shape)
+print("test_data: ", test_data.shape)
 
 # https://www.youtube.com/watch?v=p-QY7JNGD60&list=WL&index=3
 # min 16:10
